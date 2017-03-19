57ce145bd677e0beea4a7d8ce11b7a53a32a12fc
Allajah <cat.chat.katze.gatto.gato@gmail.com>
2017-03-19 16:22:02 +0900

    Load templates, scripts, images, stylesheets

---
+++ webpack.config.babel.js
@@ -0,0 +1,72 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import fs from 'fs';
+import 'babel-polyfill';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const path = require('path');
+
+let plugins = [
+    new ExtractTextPlugin({
+        filename: '[name].css'
+    }),
+    new HtmlWebpackPlugin({
+        filename: `index.html`,
+        template: `app/templates/index.pug`,
+    }),
+    new CopyWebpackPlugin([{
+        from: __dirname + `/app/img`,
+        to: __dirname + `/dist/img`
+    }]),
+];
+
+export default {
+    entry: {
+        'main': './app/scripts/main.js',
+    },
+    output: {
+        path: path.join(__dirname, "dist/"),
+        filename: "[name].bundle.js"
+    },
+    module: {
+        rules: [{
+            use: [{
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }],
+            include: [
+                path.resolve(__dirname, "scripts"),
+            ],
+            exclude: [path.resolve(__dirname, 'node_modules')]
+        }, {
+            test: /\.pug$/,
+            use: [{
+                loader: 'pug-loader'
+            }]
+        }, {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', 'sass-loader']
+            })
+        }, {
+            test: /\.(svg|woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)*$/,
+            use: {
+                loader: 'file-loader',
+                query: {
+                    name: '/fonts/[name].[ext]'
+                }
+            }
+        }]
+    },
+    resolve: {
+        modules: ["node_modules"]
+    },
+    plugins: plugins,
+    devServer: {
+        contentBase: 'dist'
+    }
+}
+
